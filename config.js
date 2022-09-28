5c1fa7d408973102c6df8e01a4f34c1a4e0f3823
Muklis12133 <muklis6763@gmail.com>
2022-09-28 14:52:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 //Ini untuk setting Bot//
 
 //『 Utama 』 //
-global.owner = ['6285896661014']  // isi jika perlu
-global.mods = ['6285664381292']   // isi jika perlu
-global.prems = ['6285664381292']  // isi jika perlu
+global.owner = ['6285838142263']  // isi jika perlu
+global.mods = ['6285838142263']   // isi jika perlu
+global.prems = ['6288274068020']  // isi jika perlu
 
 //Isi dengan nama dan nomor lu//
 global.nameowner = '⫹⫺ Muklis' // isi nama kalian bebas
-global.numberowner = '6285788657046' // isi nomor kalian dengan awalan seperti di contoh 
+global.numberowner = '6285838142263' // isi nomor kalian dengan awalan seperti di contoh 
 global.instagram = 'https://instagram.com/klis_jbff' // isi Instagram kalian harus link
 global.github = 'https://github.com/BOTCAHX' // gausah di isi juga gpp bro
 global.dana = '6285896661014' // isi dengan nomor dana yg kamu punya 
